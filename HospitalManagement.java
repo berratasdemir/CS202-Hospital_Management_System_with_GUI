e3532dfc0972cbe7d8c93ab94a08b161c8988b53
Berra Taşdemir <berra.tasdemir@ozu.edu.tr>
2024-01-06 14:18:42 +0300

    classes added

--- HospitalManagement.java
+++ HospitalManagement.java
@@ -1,7 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HospitalManagement {
     public static void main(String[] args) {
-        // Implement your main logic here
+        String url = "jdbc:mysql://localhost:3306/cs202project";
+        String user = "root";
+        String password = "B89.e637";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make a connection!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     // Methods for handling user registration and login
     // Methods for each user type (Patient, Manager, Nurse, Doctor) to perform specific actions
